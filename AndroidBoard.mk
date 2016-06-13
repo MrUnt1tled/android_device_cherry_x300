1d6501936a3d04927520db42596ccd89a4d2aef2
John Reinel Caluag <johnrenielcaluag1009@gmail.com>
2016-06-13 14:01:30 +0800

    Update to x300

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/xiaomi/hmnote1w/AndroidBoardVendor.mk
+-include vendor/cherry/x300/AndroidBoardVendor.mk
